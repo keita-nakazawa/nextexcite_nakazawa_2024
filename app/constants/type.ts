2c8e1373ab2b344f5a195c3483799e3cec5a5435
keita-nakazawa <keita.nakazawa@excite-software.net>
2024-07-29 10:56:28 +0900

    type Primitiveを修正

--- app/constants/type.ts
+++ app/constants/type.ts
@@ -1,26 +1,25 @@
-import { Assistant, AssistantTool } from "openai/resources/beta/assistants.mjs";
+import { Assistant } from "openai/resources/beta/assistants.mjs";
 import { Thread } from "openai/resources/beta/index.mjs";
 import { Run } from "openai/resources/beta/threads/index.mjs";
 import { Message } from "openai/resources/beta/threads/messages.mjs";
 import { FileObject } from "openai/resources/files.mjs";
 
-type BasicPrimitive = string | number | boolean | null | undefined | never;
-type Primitive =
-  | BasicPrimitive
-  | { [key: string]: BasicPrimitive }
-  | BasicPrimitive[]
-  | { [K in BasicPrimitive & string]: BasicPrimitive };
-
+type Primitive = string | number | boolean | symbol | bigint | null | undefined;
 /**
- * ネストしたtypeを基本型だけの表現に分割する
+ * ネストしたtypeを基本型だけの表現へと紐解く
  */
 type Flatten<T> =
-  T extends Array<infer U> ? Flatten<U>[] :
-  T extends object ? { [K in keyof T]: Flatten<T[K]> } :
-  T extends Primitive ? T :
-  // どれにも当てはまらない場合(たぶんunknownのみ)、
-  // そのままにするとtRPCが効かなくなるっぽいのでとりあえずPrimitiveとしておく
-  Primitive
+  T extends [infer First, ...infer Rest]  // タプルの場合
+  ? [Flatten<First>, ...Flatten<Rest>]
+  : T extends Array<infer U>  // 配列の場合
+  ? Flatten<U>[]
+  : T extends object  // オブジェクトの場合
+  ? { [K in keyof T]: Flatten<T[K]> }
+  : T extends Primitive  // 基本型の場合
+  ? T
+  // どれにも当てはまらない場合(たぶんunknownのみ)、そのままにするとtRPCが効かなくなるっぽいのでanyに変換しておく
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  : any;
 
 export type AssistantType = Flatten<Assistant>;
 
@@ -31,5 +30,3 @@ export type MessageType = Flatten<Message>;
 export type RunType = Flatten<Run>;
 
 export type FileObjectType = Flatten<FileObject>;
-
-export type AssistantToolType = Flatten<AssistantTool>;
