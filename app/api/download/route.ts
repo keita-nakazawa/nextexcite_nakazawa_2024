888d8a3621306a1899ca831cb0df9bbaf7139002
keita-nakazawa <keita.nakazawa@excite-software.net>
2024-08-16 23:08:52 +0900

    maxDuration=60 (Vercel用設定)

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+export const maxDuration = 60; // This function can run for a maximum of 60 seconds on Vercel
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const type = searchParams.get("type");
