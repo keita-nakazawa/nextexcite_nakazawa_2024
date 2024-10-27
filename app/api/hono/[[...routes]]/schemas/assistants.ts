c77446b62ad74ff05c3fd3718d68bf4caf67be77
keita-nakazawa <keita.nakazawa@excite-software.net>
2024-10-27 23:58:18 +0900

    Branded Types導入してみた

--- app/api/hono/[[...routes]]/schemas/assistants.ts
+++ app/api/hono/[[...routes]]/schemas/assistants.ts
@@ -2,45 +2,63 @@ import { FileObjectType } from "@/app/constants/type";
 import { z } from "zod";
 import { zfd } from "zod-form-data";
 
-export const createAssistantReqSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  instructions: z.string(),
-  model: z.string().default("gpt-4o-mini"),
-});
+export const createAssistantReqSchema = z
+  .object({
+    name: z.string(),
+    description: z.string(),
+    instructions: z.string(),
+    model: z.string().default("gpt-4o-mini"),
+  })
+  .brand("CreateAssistantReqSchema");
 
-export const retrieveAssistantReqSchema = z.object({
-  assistantId: z.string(),
-});
+export const retrieveAssistantReqSchema = z
+  .object({
+    assistantId: z.string(),
+  })
+  .brand("RetrieveAssistantReqSchema");
 
-export const addMessageReqSchema = z.object({
-  threadId: z.string(),
-  content: z.string(),
-  files: z.array(z.custom<FileObjectType>()).optional(),
-});
+export const addMessageReqSchema = z
+  .object({
+    threadId: z.string(),
+    content: z.string(),
+    files: z.array(z.custom<FileObjectType>()).optional(),
+  })
+  .brand("AddMessageReqSchema");
 
-export const runAssistantReqSchema = z.object({
-  assistantId: z.string(),
-  threadId: z.string(),
-});
+export const runAssistantReqSchema = z
+  .object({
+    assistantId: z.string(),
+    threadId: z.string(),
+  })
+  .brand("RunAssistantReqSchema");
 
-export const getRunStatusReqSchema = z.object({
-  threadId: z.string(),
-  runId: z.string(),
-});
+export const getRunStatusReqSchema = z
+  .object({
+    threadId: z.string(),
+    runId: z.string(),
+  })
+  .brand("GetRunStatusReqSchema");
 
-export const getMessagesReqSchema = z.object({
-  threadId: z.string(),
-});
+export const getMessagesReqSchema = z
+  .object({
+    threadId: z.string(),
+  })
+  .brand("GetMessagesReqSchema");
 
-export const getFirstMessageReqSchema = z.object({
-  threadId: z.string(),
-});
+export const getFirstMessageReqSchema = z
+  .object({
+    threadId: z.string(),
+  })
+  .brand("GetFirstMessageReqSchema");
 
-export const uploadFileReqSchema = z.object({
-  file: zfd.file(),
-});
+export const uploadFileReqSchema = z
+  .object({
+    file: zfd.file(),
+  })
+  .brand("UploadFileReqSchema");
 
-export const retrieveFileReqSchema = z.object({
-  fileId: z.string(),
-});
+export const retrieveFileReqSchema = z
+  .object({
+    fileId: z.string(),
+  })
+  .brand("RetrieveFileReqSchema");
